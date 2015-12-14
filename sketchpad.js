e9247ff36f4ad63bae6a486b3f1de9fc13256b73
Andrew <shope.28@osu.edu>
2015-12-14 05:59:01 -0500

    added ability to create new sketchpad with desired dimensions

--- sketchpad.js
+++ sketchpad.js
@@ -1,5 +1,35 @@
-var rows = 40;
-var columns = 40;
+var rows = 20;
+var columns = 20;
+
+function makeSketchPad(dimensions, rows, columns) {
+	// Create a grid of divs. Each div is a 'pixel' of my sketchpad
+	for (k=0; k<rows; k++) {
+		for (i=0; i<columns; i++) {
+			$("#sketchContainer").append("<div class=\"row\"></div>");
+		}
+	}
+
+	blockHeight = dimensions/rows;
+	blockWidth = dimensions/columns;
+
+	// Assign the height and width of my divs according to my
+	// variables
+	$(".row").css("width", String(blockWidth));
+	$(".row").css("height", String(blockHeight));
+}
+
+function setGradientHandler() {
+		// Event handler that changes the opacity of the divs when the
+	// cursor is over them. 
+	// For the "gradient" type of sketching
+	$(".row").hover(
+		function() {
+			$(this).css("opacity", 
+				Number($(this).css("opacity"))+.1);
+		},
+		function () {}
+		);
+}
 
 $(document).ready(function() {
 	// Get the dimensions of the containing div
@@ -9,26 +39,17 @@ $(document).ready(function() {
 	// to a number
 	padDimensions = padDimensions.slice(0, -2);
 	padDimensions = Number(padDimensions);
-	console.log(padDimensions);
 
-	blockHeight = padDimensions/rows;
-	blockWidth = padDimensions/columns;
-	
-	// Create a grid of divs. Each div is a 'pixel' of my sketchpad
-	for (k=0; k<rows; k++) {
-		for (i=0; i<columns; i++) {
-			$("#sketchContainer").append("<div class=\"row\"></div>");
-		}
-	}
+	makeSketchPad(padDimensions, rows, columns);
+	setGradientHandler();
 
-	$(".row").css("width", String(blockWidth));
-	$(".row").css("height", String(blockHeight));
-
-	$(".row").hover(
+	$("#newPadButton").click(
 		function() {
-			$(this).css("opacity", 
-				Number($(this).css("opacity"))+.1);
-		},
-		function () {}
-		);
+			$(".row").remove();
+			rows = $(".dimBoxes").val();
+			columns = rows;
+			console.log(rows);
+			makeSketchPad(padDimensions, rows, columns);
+			setGradientHandler();
+		});
 });
